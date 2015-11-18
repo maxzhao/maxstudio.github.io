2a1f016839feb0711e67aae13bd8a94ffdf58748
maxzhao <zhaoguowei@gmail.com>
2015-11-18 10:39:34 +0800

    fix

---
+++ shi/injections/domob.js
@@ -0,0 +1,113 @@
+(function () {
+
+    function checkReply(a) {
+        if (!a)
+            return !1;
+        if ("string" == typeof a)
+            try {
+                a = JSON.parse(a)
+            } catch (b) {
+                return !1
+            }
+        return "error"in a ? !1 : a
+    }
+
+    function getPlayItems() {
+
+        if ((window.queryList || window.eggQuest > 0)
+        	&& window.lockCount < 5){
+        		window.lockCount++;
+            return;
+        }
+
+        window.lockCount = 0;
+
+        window.queryList = true;
+        $.ajax({
+            url: window.actionUrl.getTaskList.url,
+            type: window.actionUrl.getTaskList.type,
+            data: getCommonReqData(),
+            complete: function (xhr) {
+                 window.queryList = false;
+            },
+            success: function (b) {
+
+                window.top.shidata["domob"].dotNum++;
+
+                if (b.list.length > 0) {
+                    for (var i = b.list.length - 1; i >= 0; i--) {
+                        var task = b.list[i];
+                        if (task.type != 3
+                            && !task.received && task.restNum > 0) {
+
+                            window.eggQuest++;
+
+                            $.ajax({
+                                url: window.actionUrl.getAdDetail.url,
+                                type: window.actionUrl.getAdDetail.type,
+                                data: $.extend(getCommonReqData(), {
+                                    id: task.id,
+                                    type: task.type
+                                }),
+                                complete: function (xhr) {
+                                    if (!xhr.continueCode) {
+                                        window.eggQuest--;
+                                    }
+                                },
+                                success: function(a, status, xhr) {
+                                    a = checkReply(a);
+
+                                    if(!a)
+                                        return;
+
+                                    xhr.continueCode = true;
+
+                                    var turl = a.crp_url;
+                                    if(turl.indexOf("?") > 0)
+                                        turl+=getCommonParams().substring(1) + "&ts=" + getCurTime();
+                                    else
+                                        turl+= getCommonParams() + "&ts=" + getCurTime();
+                                    $.ajax({
+                                        url: a.crp_url,
+                                        data: turl,
+                                        type: "post",
+                                        success: function (a) {
+                                            window.top.shidata["domob"].currentEgg++;
+                                        },
+                                        complete: function (xhr) {
+                                            window.eggQuest--;
+                                        }
+                                    });
+                                }
+                            }).continueCode = false;
+
+                        }
+                    }
+                }
+            }
+        });
+
+        function getRandomInt(min, max) {
+            var timeMinute = (new Date()).getMinutes();
+            if ((timeMinute > 28 && timeMinute < 32)
+                || (timeMinute < 2 && timeMinute > 58))
+                return min;
+            return Math.floor(Math.random() * (max - min)) + min;
+        }
+
+        clearInterval(window.queryListID);
+        window.queryListID = setInterval(getPlayItems, getRandomInt(500, 2000));
+    }
+
+    window.top.shidata["domob"] = {currentEgg:0, dotNum:0};
+
+    window.lockCount = 0;
+    window.queryList = false;
+    window.eggQuest = 0;
+    window.lastEgg = 0;
+    window.queryListID = setInterval(getPlayItems, 500);
+
+
+})();
+
+
---
+++ shi/injections/itry.js
@@ -0,0 +1,104 @@
+(function(){
+
+    window.btnStatus = function(user_id,order_id,appid,detail_url){
+    	$.ajax({
+            type : "post",
+            url : "/itry/getIsDownApp",
+            data : {appid:appid,user_id:user_id},
+            dataType: 'text',
+            async : false,
+            complete : function(xhr) {
+            	if(!xhr.continueCode){
+            		window.eggQuest--;
+            	}
+            },
+            success : function(back, status, xhr){
+            	if(back=="-1"){
+            	}else if(isNumber(back)){
+            			xhr.continueCode = true;
+                	$.ajax({
+                        type : "post",
+                        url : "/itry/user_click_record",
+                        data : {appid:appid,user_id:user_id,order_Id:order_id,type:"app"},
+                        dataType: 'text',
+                        async : false,
+                        complete : function(){
+                        	window.eggQuest--;
+                        },
+                        success : function(num){
+                        	if(num=="-1"){
+                        	}else if(isNumber(num)){
+                        		var val = ''+appid+order_id+user_id;
+                            	var dateClick = getStore(val); //开始时间
+                        		var minutes = compareMinutes(dateClick);
+                            	if(typeof(dateClick)=="undefined" || minutes>=60){
+                            		setStore(val,new Date().getTime());
+                            	}
+                            	window.top.shidata["itry"].currentEgg++;
+                        	}
+                        }
+                    });
+                }
+           	 }  
+        }).continueCode = false;
+    }
+
+
+	function getPlayItems(){
+
+        if ((window.queryList || window.eggQuest > 0)
+            && window.lockCount < 5){
+                window.lockCount++;
+            return;
+        }
+
+        window.lockCount = 0;
+
+		window.queryList = true;	
+		 $.get("/itry/appList?refresh=" + Date.now(), {}, function (back) {
+
+		 	window.top.shidata["itry"].dotNum++;
+
+        var doc = $(back);	
+        var invertELs = doc.find(".app_j");
+        if(invertELs.length > 0){
+        	invertELs.each(function(i, e){
+
+                var priceEls = $("span.price", e);
+                if(priceEls.length > 0
+                    && priceEls[0].text() == "进行中")
+                    return;
+            
+                window.eggQuest++;
+        		e.onclick();
+        	});
+        }
+
+        doc.remove();
+
+		}, "html").always(function(){
+			window.queryList = false;
+		});
+
+
+		function getRandomInt(min, max) {
+			var timeMinute = (new Date()).getMinutes();
+			if((timeMinute > 28 && timeMinute < 32)
+				|| (timeMinute < 2 && timeMinute > 58))
+				return min;
+  		return Math.floor(Math.random() * (max - min)) + min;
+		}
+
+		clearInterval(window.queryListID);
+		window.queryListID = setInterval(getPlayItems, getRandomInt(500, 2000));
+	}
+
+	window.top.shidata["itry"] = {currentEgg:0, dotNum:0};
+
+    window.lockCount = 0;
+	window.queryList = false;
+	window.eggQuest = 0;
+	window.lastEgg = 0;
+	window.queryListID = setInterval(getPlayItems, 500);
+
+})();
