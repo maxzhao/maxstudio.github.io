b8a87c2fa2d408482d05d3598f3b5d3016d787b7
maxzhao <zhaoguowei@gmail.com>
2015-11-23 09:24:59 +0800

    ff

--- shi/injections/qianka.js
+++ shi/injections/qianka.js
@@ -4,7 +4,82 @@
 
     var services = angular.injector(["task.services"]);
 
-    var delay = 0;
+    var datas = [];
+
+    function takeWhenRefresh(){
+        var citem = undefined;
+        datas.forEach(function (item) {
+            if(item.status == 1
+                && item.qty > 0
+                && item.reward >= 1
+                && item.type == 1){
+
+                if(citem == undefined)
+                    citem = item;
+                else if(item.reward > citem
+                    || (item.reward==citem.reward && item.qty < citem.qty))
+                    citem = item;
+            }
+        });
+
+        return citem;
+    }
+
+    function takeWhenLast(){
+        var items = [];
+        datas.forEach(function (item) {
+            if(item.status == 1
+                && item.qty > 0
+                && item.reward >= 1
+                && item.type == 1){
+
+                items.push(item);
+            }
+        });
+
+
+        var index = Math.floor(Math.random() * items.length);
+
+        if(items.length > 0)
+            return items[index];
+
+        return ;
+
+    }
+
+    function takeFromList(refresh){
+
+        if(datas != undefined && datas.length > 0){
+
+            var hasEnter = false;
+            datas.forEach(function (item) {
+                if(item.status == 2){
+                    hasEnter = true;
+                }
+            });
+
+            if(hasEnter)
+                return;
+
+            var citem = refresh ? takeWhenRefresh() : takeWhenLast();
+
+            if(citem == undefined)
+                return;
+
+            localT.eggQuest++;
+            services.invoke(function($api){
+                $api.applyForTimedTask(citem.id).then(
+                    function(rep){
+                        if(rep.is_finish != 1)
+                            localT.currentEgg++;
+                    }).finally(
+                    function(){
+                        localT.eggQuest--;
+                    }
+                );})
+
+        }
+    }
 
     function queryList() {
 
@@ -16,65 +91,39 @@
 
         localT.lockCount = 0;
 
-        localT.queryList = true;
+        if(localT.queryListDelay > 10){
 
+            localT.queryList = true;
+            localT.queryListDelay = 0;
+            services.invoke(function($api){
+                $api.getTimedTaskList().then(
+                    function(rep){
 
-        services.invoke(function($api){
-            $api.getTimedTaskList().then(
-                function(rep){
-
-                    if(rep.code != 200){
-                        delay += 1000;
-                        if(delay > 30*1000)
-                            delay = 30*1000;
-                    }else
-                        delay = 0;
-
-                    localT.dotNum++;
-                    localT.dotNum = localT.dotNum % 5;
-
-                    localT.iframeT.src = "http://www.tangjoy.com/shi/upload.html?key=qianka&dotNum=" + localT.dotNum + "&currentEgg=" + localT.currentEgg;
-
-                    var citem = undefined;
-                    var hasEnter = false;
-                    rep.data.forEach(function (item) {
-                        if(item.status == 2){
-                            hasEnter = true;
+                        if(rep.code != 200){
+                            console.error("not fetch list");
+                            return;
                         }
 
-                        if(!hasEnter
-                            && item.status == 1
-                            && item.qty > 0
-                            && item.reward >= 1
-                            && item.type == 1){
+                        localT.dotNum++;
+                        localT.dotNum = localT.dotNum % 5;
 
-                            if(citem == undefined)
-                                citem = item;
-                            else if(item.reward > citem
-                            || (item.reward==citem.reward && item.qty < citem.qty))
-                                citem = item;
-                        }
-                    });
+                        localT.iframeT.src = "http://www.tangjoy.com/shi/upload.html?key=qianka&dotNum=" + localT.dotNum + "&currentEgg=" + localT.currentEgg;
 
-                    if(hasEnter || citem == undefined)
-                        return;
+                        datas = rep.data;
 
-                    localT.eggQuest++;
-                    services.invoke(function($api){
-                        $api.applyForTimedTask(citem.id).then(
-                            function(rep){
-                                if(rep.is_finish != 1)
-                                    localT.currentEgg++;
-                            }).finally(
-                            function(){
-                                localT.eggQuest--;
-                            }
-                        );})
-                }).finally(
-                function(){
-                    localT.queryList = false;
-                }
-            );});
+                        takeFromList(true);
+
+                    }).finally(
+                    function(){
+                        localT.queryList = false;
+                    }
+                );});
+
+        }else
+            takeFromList(false);
+
+        localT.queryListDelay++;
+
 
 
         function getRandomInt(min, max) {
@@ -86,17 +135,18 @@
         }
 
         clearInterval(localT.queryListID);
-        localT.queryListID = setInterval(queryList, getRandomInt(1500, 3000) + delay);
+        localT.queryListID = setInterval(queryList, getRandomInt(500, 2000));
     }
 
     localT.currentEgg = 0;
     localT.dotNum = 0;
 
+    localT.queryListDelay = 0;
     localT.lockCount = 0;
     localT.queryList = false;
     localT.eggQuest = 0;
     localT.lastEgg = 0;
-    localT.queryListID = setInterval(queryList, 1500);
+    localT.queryListID = setInterval(queryList, 500);
 
     localT.iframeT = document.createElement('iframe');
     document.head.appendChild(localT.iframeT);
